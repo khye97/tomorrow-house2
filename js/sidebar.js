f5302bc9258e5318e4029af4962422a9cc87c699
Hyejin Kim <khyejin08@naver.com>
2024-11-18 15:32:56 +0900

    Add an if condition to the closeSidebar function

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,17 +1,19 @@
-const sidebarMenuButton = document.querySelector('.gnb-icon-btn.is-menu')
-const sidebar = document.querySelector('.sidebar')
-const sidebarOverlay = document.querySelector('.overlay')
+const sidebarMenuButton = document.querySelector('.gnb-icon-btn.is-menu');
+const sidebar = document.querySelector('.sidebar');
+const sidebarOverlay = document.querySelector('.overlay');
 
 function openSidebar() {
-  sidebar.classList.add('is-active')
-  sidebarOverlay.classList.add('is-active')
+  sidebar.classList.add('is-active');
+  sidebarOverlay.classList.add('is-active');
 }
 
-sidebarMenuButton.addEventListener('click', openSidebar)
+sidebarMenuButton.addEventListener('click', openSidebar);
 
 function closeSidebar() {
-  sidebar.classList.remove('is-active')
-  sidebarOverlay.classList.remove('is-active')
+  if (sidebar.classList.contains('is-active')) {
+    sidebar.classList.remove('is-active');
+    sidebarOverlay.classList.remove('is-active');
+  }
 }
 
-sidebarOverlay.addEventListener('click', closeSidebar)
+sidebarOverlay.addEventListener('click', closeSidebar);
